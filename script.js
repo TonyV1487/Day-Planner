fd86992f84864bf85e1f206f5c5a470c36aa407b
TonyV <tvester1487@gmail.com>
2019-10-15 17:53:15 -0500

    Initiating files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Setup var
+var calendarLength = 24;
+
+// Fill <div> with all of the time slots
+function fillTodoList() {
+  var todoList = $('#todoList');
+  for(i=0; i<(calendarLength+1); i++)
+}
+//
