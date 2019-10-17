8a1dbaeebc4ebd4013b4a1a5ba7e97758538ded7
TonyV <tvester1487@gmail.com>
2019-10-17 09:41:15 -0500

    Commit to review code for Bootstrap formating issues

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-// Setup var
-var calendarLength = 24;
+$(document).ready(function() {
+  // Setup var
+  var calendarLength = 24;
 
-// Fill <div> with all of the time slots
-function fillTodoList() {
-  var todoList = $('#todoList');
-  for(i=0; i<(calendarLength+1); i++)
-}
-//
+  // Fill <div> with all of the time slots
+  function createPlanner() {
+    console.log('run');
+    for (i = 0; i < calendarLength; i++) {
+      console.log('run2');
+      // Create date div
+      var dateDiv = $(`<div class="row">`);
+      console.log(dateDiv);
+      // Create create columns
+      var timeCol = $(`<div class="col-2">Col 1</div>`);
+      var taskCol = $(`<div class="col-8">Col 2</div>`);
+      var saveCol = $(`<div class="col-2">Col 3</div>`);
+
+      // Append
+
+      dateDiv.append(timeCol);
+      dateDiv.append(taskCol);
+      dateDiv.append(saveCol);
+      $('#todoList').append(dateDiv);
+    }
+  }
+  createPlanner();
+});
